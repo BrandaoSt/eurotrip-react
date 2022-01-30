e9ea08417d54c08897ce812bdc478beabb7547fc
BrandaoSt <patriciabrandao7@gmail.com>
2022-01-30 19:41:14 -0300

    Updates

---
+++ precache-manifest.23d5471d902f698e145e2b8e3d84c43f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1c5ed2038290c4d00847b3ace206fd",
+    "url": "/eurotrip-react/index.html"
+  },
+  {
+    "revision": "8f5f6bcda436da1d0bf0",
+    "url": "/eurotrip-react/static/css/2.adc0603d.chunk.css"
+  },
+  {
+    "revision": "1b39d89db266e8228f51",
+    "url": "/eurotrip-react/static/css/main.cd553c86.chunk.css"
+  },
+  {
+    "revision": "8f5f6bcda436da1d0bf0",
+    "url": "/eurotrip-react/static/js/2.8b2cb2dd.chunk.js"
+  },
+  {
+    "revision": "39c7535066ffd7f7e8fb",
+    "url": "/eurotrip-react/static/js/3.16e4402c.chunk.js"
+  },
+  {
+    "revision": "1b39d89db266e8228f51",
+    "url": "/eurotrip-react/static/js/main.53460697.chunk.js"
+  },
+  {
+    "revision": "8dbdddafe522c19bde3f",
+    "url": "/eurotrip-react/static/js/runtime~main.d3153558.js"
+  },
+  {
+    "revision": "3da11fbdba8426b98d01d7acdaef0d23",
+    "url": "/eurotrip-react/static/media/avatar1.3da11fbd.png"
+  },
+  {
+    "revision": "cd623f124a73276e0d7829e6d232883d",
+    "url": "/eurotrip-react/static/media/avatar2.cd623f12.png"
+  },
+  {
+    "revision": "1aa320f6c9e64369e5585c646c1f0577",
+    "url": "/eurotrip-react/static/media/avatar4.1aa320f6.png"
+  },
+  {
+    "revision": "87d2c00b734a497d63d2c973080c3095",
+    "url": "/eurotrip-react/static/media/avatar5.87d2c00b.png"
+  },
+  {
+    "revision": "9ae5374ac508e1c23051eaf47fee44b5",
+    "url": "/eurotrip-react/static/media/bg.9ae5374a.jpg"
+  },
+  {
+    "revision": "adc91222736438e4db1fd9d3c3090475",
+    "url": "/eurotrip-react/static/media/bg3.adc91222.jpg"
+  },
+  {
+    "revision": "d0b0a7b03e0643be3ddb9451cd9bb709",
+    "url": "/eurotrip-react/static/media/espanha3.d0b0a7b0.jpg"
+  },
+  {
+    "revision": "50b29fccba950de0135e2f3dd4130301",
+    "url": "/eurotrip-react/static/media/franca5.50b29fcc.jpg"
+  },
+  {
+    "revision": "73e51f741aef29c046989152fc2a11c6",
+    "url": "/eurotrip-react/static/media/holanda6.73e51f74.jpg"
+  },
+  {
+    "revision": "5af4cef63e50bcb9d01b40cb7e796c8b",
+    "url": "/eurotrip-react/static/media/inglaterra2.5af4cef6.jpg"
+  },
+  {
+    "revision": "fb6c8d20407dccb1981a93036d083b87",
+    "url": "/eurotrip-react/static/media/minhafoto2.fb6c8d20.jpg"
+  },
+  {
+    "revision": "eaa6b9a4a6de0c48d5be944b8ef09df9",
+    "url": "/eurotrip-react/static/media/portugal1.eaa6b9a4.jpg"
+  },
+  {
+    "revision": "a4b2de15d78722f8f3d15b7f54713eb6",
+    "url": "/eurotrip-react/static/media/roma.a4b2de15.jpg"
+  },
+  {
+    "revision": "185b23c8fd19135f09dabcd6e1c143c1",
+    "url": "/eurotrip-react/static/media/sintra8.185b23c8.jpg"
+  },
+  {
+    "revision": "fafa3a67aa1055498d0c223286d0ffaf",
+    "url": "/eurotrip-react/static/media/veneza.fafa3a67.jpg"
+  }
+]);
